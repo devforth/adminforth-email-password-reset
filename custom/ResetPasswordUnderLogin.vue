66c3980b614c855cf5e631eb472a2f7c65580a00
Ivan Borshchov <ivan@devforth.io>
2025-01-09 22:47:58 +0200

    initial version

---
+++ custom/ResetPasswordUnderLogin.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="text-gray-500 dark:text-gray-400 font-sm text-left mt-3">
+    {{$t('Forgot password?')}} 
+    <Link to="/reset-password">
+      {{$t('reset it')}}
+    </Link>
+  </p>
+</template>
+
+<script setup>
+import Link from '@/afcl/Link.vue';
+
+</script>
